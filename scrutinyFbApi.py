f3fb7349ee2d005d104397531b24e3ea3281ff4a
Shanmukha Srinivasan <shanmukha@Shanmukhas-MacBook-Pro-2.local>
2019-11-11 15:00:34 -0500

    Modified configuration, fixed timeout issue

--- scrutinyFbApi.py
+++ scrutinyFbApi.py
@@ -1,5 +1,5 @@
 from os import environ
-from flask import Flask
+from flask import Flask, jsonify, request
 import mysql.connector
 
 app = Flask(__name__)
@@ -7,12 +7,27 @@ app = Flask(__name__)
 cnx = mysql.connector.connect(user=environ.get('user'), password=environ.get('pass'),
                               host=environ.get('host'),
                               port=environ.get('port'),
-                              database=environ.get('database'))
+                              database=environ.get('database'),
+                              connection_timeout=10)
+
+#def getPlayerInfo(playerID):
+#    mycursor = cnx.cursor()
+#    mycursor.execute("SELECT * from playerInfo WHERE player_id = '/players/M/McCaCh01'")
+#    myresult = mycursor.fetchall()
+#    return myresult[0]
 
 @app.route('/')
-def index():
-	#return 'Welcome to our ScrutinyFB API!'
-	return environ.get('database')
+def welcome():
+    return 'Welcome to Scrutiny FB API! Hosted by Heroku, documentation to be released on my github soon!'
+
+@app.route('/getPlayerId')
+def hello():
+#    playerID = request.args.get('player_id')
+#    return jsonify(database_hello(playerID))
+    mycursor = cnx.cursor()
+    mycursor.execute("SELECT * from playerInfo WHERE player_id = '/players/M/McCaCh01'")
+    myresult = mycursor.fetchall()
+    return jsonify(myresult[0])
 
 if __name__ == "__main__":
 	app.run()
